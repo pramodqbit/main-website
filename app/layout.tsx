9aa354933d8e3d1575eb55871d843b9fc89b9829
Adil Hussain <adil.hussain@brightedge.com>
2025-10-04 22:38:41 +0530

    Add @vercel/speed-insights dependency and integrate SpeedInsights component into the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import SmoothScrolling from "@/components/animation/smooth-scrolling";
 
 const geistSans = Geist({
@@ -32,6 +33,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
+			<SpeedInsights />
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} ${sevenSegment.variable} antialiased`}>
 				<SmoothScrolling />
